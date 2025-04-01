c2ee66df4cfe8ceaa0c7b3d2e379cb46bea77354
TUSHAR-DALVI-09 <tushardalvi099@gmail.com>
2025-04-02 00:30:18 +0530

    working on progress

---
+++ app.js
@@ -0,0 +1,25 @@
+let a=document.getElementById("img");
+
+function myFunOff(){
+    a.style.animationDuration = 0+"s";
+}
+
+function myFun1(){
+    a.style.animationDuration = 1+"s";
+}
+
+function myFun2(){
+    a.style.animationDuration = 0.8+"s";
+}
+
+function myFun3() {
+    a.style.animationDuration = 0.5+"s";
+}
+
+function myFun4(){
+    a.style.animationDuration=0.3+"s"
+}
+
+function myFun5(){
+    a.style.animationDuration = 0.1+"s";
+}
